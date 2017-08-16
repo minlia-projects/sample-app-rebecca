b5bdd1d9485c3b87448cabbc713938a0de150852
Henry Lin <badcop@163.com>
2017-08-16 21:06:50 +0800

    delete files other than rebecca

--- playground/src/main/java/localhost/starwebservice/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * StarWebService
- * 
- */
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://localhost/StarWebService/", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package localhost.starwebservice;

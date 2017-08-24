6a47bbb9caf178a352cf3732960efd924f493f10
will <will@minlia.com>
2017-08-25 03:09:39 +0800

    Update at 20170825030922 by dev/will

--- src/main/resources/config/old-data-mysql.sql
+++ src/main/resources/config/old-data-mysql.sql
@@ -1,3 +1,4 @@
+-- mysqldump --complete-insert -uroot minlia_cloud_sample_rebecca_dev_v1 > target/data-mysql.sql
 INSERT INTO `role` (`id`, `created_by`, `created_date`, `enabled`, `last_modified_by`, `last_modified_date`, `code`, `label`) VALUES (1,'10000','2016-08-25 02:24:09.000000','','10000','2017-08-25 02:24:11.000000','ROLE_ADMIN','System Administrator');
 INSERT INTO `user` (`id`, `created_by`, `created_date`, `enabled`, `last_modified_by`, `last_modified_date`, `account_expired`, `cellphone`, `credentials_expired`, `email`, `expired`, `first_name`, `full_name`, `guid`, `last_name`, `locked`, `nick_name`, `password`, `username`) VALUES (1,'10000','2016-08-25 02:23:53.000000','','10000','2017-08-25 02:23:57.000000',0,'15692015683',0,'admin@minlia.com',0,'admin','administrator','10000','istrator',0,'admin','$2a$10$wpizgM9q0V4j3stnP.GS/u/ZJeTfqBshPxc2v5u93IY3c9t89D0Re','admin');
 INSERT INTO `map_user_roles` (`user_id`, `role_id`) VALUES (1, 1);
